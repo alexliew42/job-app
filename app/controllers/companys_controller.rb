c41bcaf7fb83384702148c6fa7825032a1fa4a9e
Chazz Gagliano <cgags17285@gmail.com>
2023-04-27 13:58:59 -0400

    add create full stack method

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -6,4 +6,13 @@ class CompanysController < ApplicationController
     @company = Company.find_by(id: params[:id])
     render :show
   end
+  def create
+    @company = Company.new(
+    name: params[:company][:name],
+    logo: params[:company][:logo],
+    description: [:company][description]
+    )
+    @company..save
+    redirect_to "/company/#{@company.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   get "/companys" => "companys#index"
   get "/companys/:id" => "companys#show"
-=======
+  
   resources :jobs
   post "/users" => "users#create"
   post "/sessions" => "sessions#create"
->>>>>>> 45b35ef3ed2331400ddf1802071ac182a0604c92
+
 end
