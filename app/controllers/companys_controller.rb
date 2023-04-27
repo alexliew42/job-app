2b94ea63c7210ddd7064c7903c59ba6bbea0cb2c
Chazz Gagliano <cgags17285@gmail.com>
2023-04-27 13:39:11 -0400

    create show action

--- app/controllers/companys_controller.rb
+++ app/controllers/companys_controller.rb
@@ -1,6 +1,9 @@
 class CompanysController < ApplicationController
   def index
     @companys = Company.all
-    
+  end
+  def show
+    @company = Company.find_by(id: params[:id])
+    render :show
   end
 end
