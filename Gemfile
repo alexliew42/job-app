75b4d2a9604fc903b13fdaf2d42940e0bc396895
Alex Liew <liewtwoo42@gmail.com>
2023-04-27 14:29:17 -0400

    database

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ end
 
 
 gem 'jwt'
+
+gem "seed_dump"
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_163931) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_27_164651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,11 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_163931) do
     t.string "name"
     t.string "logo"
     t.string "description"
-=======
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_164258) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.integer "company_id"
@@ -33,17 +30,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_164258) do
     t.string "location"
     t.boolean "active"
     t.string "salary_range"
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_164651) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
->>>>>>> 63e1aa525a55475c38b0ece8618f472f1440eb9a
->>>>>>> 45b35ef3ed2331400ddf1802071ac182a0604c92
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
