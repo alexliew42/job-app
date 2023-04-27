415f94bc7f499c1ba8ce7694f1e975f92a11d66f
Alex Liew <liewtwoo42@gmail.com>
2023-04-27 16:42:24 -0400

    create sessions and users html.new.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
+  # has_secure_password
+  attr_accessor :email, :name, :password, :password_confirmation
   validates :email, presence: true, uniqueness: true
 end
