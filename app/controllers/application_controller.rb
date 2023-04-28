230c5aab8a3116a58eb946e54933c915646de6ab
Alex Liew <liewtwoo42@gmail.com>
2023-04-28 14:33:37 -0400

    fix merge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
 
+  def authenticate_admin
+    unless current_user && current_user.admin
+      render json: {message: "Request Denied"}, status: :unauthorized
+    end
+  end
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
@@ -11,29 +16,5 @@ class ApplicationController < ActionController::Base
     redirect_to '/login', status: :see_other unless current_user
   end
 
-
-  # def current_user
-  #   auth_headers = request.headers["Authorization"]
-  #   if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
-  #     token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
-  #     begin
-  #       decoded_token = JWT.decode(
-  #         token,
-  #         Rails.application.credentials.fetch(:secret_key_base),
-  #         true,
-  #         { algorithm: "HS256" }
-  #       )
-  #       User.find_by(id: decoded_token[0]["user_id"])
-  #     rescue JWT::ExpiredSignature
-  #       nil
-  #     end
-  #   end
-  # end
-
-  # def authenticate_user
-  #   unless current_user
-  #     render json: {}, status: :unauthorized
-  #   end
-  # end
 
 end
